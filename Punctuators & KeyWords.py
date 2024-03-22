976b143301334d53ad9cb04ef9e82d9b748ad48f
Kumari Neha <158587756+Kneha256@users.noreply.github.com>
2024-03-22 12:02:35 +0530

    Add files via upload

---
+++ Punctuators & KeyWords.py
@@ -0,0 +1,25 @@
+#TYPES OF TOCKENS IN PYTHON
+#1)Identifiers
+#2)Literals
+#3)Operators
+#4)Keywords-reserved values
+#Can not be use as a identifier or variables
+#case Sensitive(upper case and lower case)
+
+#printsum
+a = 2
+b = 3
+sum = a + b
+print(sum)
+
+#printdiff
+a = 5
+b = 3
+diff = a - b
+print(diff)
+
+#5)Punctuators-(),[],{},#,@,etc.
+#they are symbols use to organise sentence structure in programming
+#=,-=,+=,/=,*=,//=,etc.
+#python is a implicit typed language(no need to tell type of the variables)
+#ex-java.cpp,c are explicit typed Languages.(int age = 23).
