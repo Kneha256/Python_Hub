8b307bef3da8b98bbe6942cc40125d96d91edb0a
Kumari Neha <158587756+Kneha256@users.noreply.github.com>
2024-03-22 11:19:13 +0530

    Add files via upload

---
+++ DataTypes & Variables.py
@@ -0,0 +1,53 @@
+#developed by guido van rossum
+#easy to understnd 
+#free and open source
+#portable
+
+#to print output
+print("Hello World")
+print("Kumari Neha")
+print("Hello World","Kumari Neha")
+
+#variables-name give to a memory location, can be changes
+#rules for identifiers
+#upper case, lower case, underscore, digit
+#cant not start with digit
+#special symbols can not ne use(!,@,#,%,$,etc)
+#any length
+#simple, short, meaningful and readable(count,sum,name,age,etc)
+
+name = "neha"
+age = 23
+price = 23.99
+print(name)
+print(age)
+print(price)
+
+age = 34
+age2 = age
+print(age2)
+
+#type of (data type)
+print(type(name))
+print(type(age2))
+print(type(price))
+
+#Data Types in Python
+#1)Integer(+,-,0) [int type]
+#2)String(sentance)
+print("sk")
+print('sk')
+print('''sk''')
+
+#3)Float(decimal values)
+print(12.45)
+print(type(12.45))
+
+#4)Boolean()-True,False
+old = False
+print(old)
+print(type(age))
+
+#5)None-no value give to a location
+a = None
+print(type(a))
