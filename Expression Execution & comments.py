030b8a572b7ff3a532e8b281c87dde347463b111
Kumari Neha <158587756+Kneha256@users.noreply.github.com>
2024-03-24 13:15:25 +0530

    Add files via upload

--- Expression Execution & comments.py
+++ Expression Execution & comments.py
@@ -30,3 +30,47 @@ print(c)
     #or
 a,b=2,5.0
 print(a*b)
+
+#division operator with two integer will give float value
+a,b=1,2
+print(a/b) 
+
+#integer division-(//) with float, gives int value but display as float
+a,b=1.5,3
+c=a//b
+print(c, a/b)
+
+a,b=2,1
+print(a//b)
+
+#floor gives closest int, which is lesser or equal to the float value
+#(a//b) is same as floor(a/b)
+
+a,b=4,2
+print(a/b)
+   #and
+a,b=4,2
+print(a//b)
+
+a,b=-12,5
+print(a/b)
+   #and
+a,b=-12,5
+print(a//b)
+
+#remainder is negative when denominator is -ve 
+a,b=3,2
+print(a%b)
+
+a,b=-3,2
+print(a%b)
+
+a,b=16,-5
+print(a%b)
+
+a,b=5,-2
+print(a%b)
+
+#comments-won't be printed (#)
+
+
