63ff30c5e9b9f50b307fc492c73b11c31e7bf6c7
Kumari Neha <158587756+Kneha256@users.noreply.github.com>
2024-03-26 02:08:29 +0530

    Add files via upload

---
+++ Conditional Statements_1.py
@@ -0,0 +1,21 @@
+#if-elif-else or if or if-elif(Syntax)
+Light=input("Light : ")
+if(Light == "red"):
+    print("Stop")
+elif(Light == "green"):
+    print("Go")
+elif(Light == "yellow"):
+    print("Look")
+else:
+    print("Broken")
+
+
+Marks=int(input("Enter your Marks : "))
+if(Marks>=90):
+    print("Grade : A")
+elif(Marks<=90 and Marks>=80):
+    print("Grade : B")
+elif(Marks<=80 and Marks>=70):
+    print("Grade : C")
+else:
+    print("Grade : D")
