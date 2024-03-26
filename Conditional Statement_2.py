75460f70cb279e6307fffc0f9b09abec0abf07f8
Kumari Neha <158587756+Kneha256@users.noreply.github.com>
2024-03-26 21:24:40 +0530

    Add files via upload

--- Conditional Statement_2.py
+++ Conditional Statement_2.py
@@ -4,26 +4,3 @@
 food=input("food : ")
 eat="yes" if food == "cake" else "no"
 print(eat)
-
-#<stt1> if <condition> else <stt2>
-food=input("food: ")
-print("sweet") if food == "cake" or food == "jalebi" else print("no sweet")
-
-      #or
-
-#if-elif-else
-food=input("food : ")
-if(food=="cake" or food=="jalebi"):
-    print("sweet")
-else:
-    print("no sweets")
-
-
-#clever if / ternary operator
-age=int(input("age : "))
-vote=("yes", "no") [age<18]
-print(vote)
-
-sal=float(input("salary : "))
-tax=sal*(0.1, 0.2) [sal>=50000]
-print(tax)
