23e8fa071795cc8f7475148a86d7d9381e3203b7
Kumari Neha <158587756+Kneha256@users.noreply.github.com>
2024-03-27 10:46:42 +0530

    Add files via upload

---
+++ TypeConversion & TypeCasting.py
@@ -0,0 +1,22 @@
+#conversion-from one data type to another data type
+#type casting - manual conversion
+a=int("2")
+b=4.6
+print(a+b)
+print(type(a))
+
+a=3.24
+a=str(a)
+print(type(a))
+
+a="2"
+b=str(4.7)
+print(a+b)
+
+#Type conversion - automatically conversion
+a=4
+b=5.6
+sum=a+b
+print(sum)
+print(type(a))
+
