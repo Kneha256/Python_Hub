2feab874680378018b557efc3bda1a315e9c121c
Kumari Neha <158587756+Kneha256@users.noreply.github.com>
2024-04-03 23:37:41 +0530

    Update String.py

--- String.py
+++ String.py
@@ -134,6 +134,31 @@ print(str1.count("o")) #4
 
 
 
+#Practice Question
+num1=int(input("Enter First number : "))
+num2=int(input("Enter second number : "))
+print("sum = ",num1+num2)
+
+side=float(input("Enter length of a side : "))
+area=side*side #or area=side**2
+print(area)
+
+num1=float(input("num1 : "))
+num2=float(input("num2 : "))
+average=(num1+num2)/2
+print(average)
+
+a=int(input("num1 = "))
+b=int(input("num2 = "))
+print("True") if a>=b else print("False")
+        #or
+a=int(input("num1 = "))
+b=int(input("num2 = "))
+c=("False", "True") [a>=b]
+print(c)
+
+
+
 
 
             
