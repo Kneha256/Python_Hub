7debd440549bfd33639a843a803d903c603770d5
Kumari Neha <158587756+Kneha256@users.noreply.github.com>
2024-04-03 23:45:26 +0530

    Add files via upload

---
+++ Practice Question_2.py
@@ -0,0 +1,25 @@
+#Question01
+first_name=input("Enter your first name : ") #neha
+print(len(first_name)) #4
+
+#Question02-write a programe to give the no of occurance of "$"
+str = "I am learning python from $shraddha mam"
+print(str.count("$")) #1
+
+str1="apna college"
+print(len(str1)) #12
+print(str1[2]) #n
+print(str1[1:6]) #pna c
+print(str1[-11:-6]) #pna c
+
+
+#string functions practice
+str = "i am learning python from shraddha mam"
+print(str.endswith("am")) #True
+print(str.capitalize()) #I am learning python from shraddha mam
+print(str.replace("mam", "khapra")) #i am learning python from shraddha khapra
+print(str.find("from")) #21
+print(str.count("a")) #5
+
+
+
