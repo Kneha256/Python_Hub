9d79b98c554ba799d6fb4b81e8888c3e4582c0ff
Kumari Neha <158587756+Kneha256@users.noreply.github.com>
2024-04-05 11:03:01 +0530

    Add files via upload

--- Conditional Statements.py
+++ Conditional Statements.py
@@ -1,4 +1,8 @@
-#if-elif-else or if or if-elif (Syntax)
+#if-elif-else or if or if-elif(Syntax)
+#"elif" condition will only execute if "if" condition is false.
+#there can be number of if and elif statement but only one else statement.
+#indentation-proper spacing
+
 Light=input("Light : ")
 if(Light == "red"):
     print("Stop")
@@ -10,15 +14,55 @@ else:
     print("Broken")
 
 
+
+
+marks=int(input("Enter your marks : "))
+if(marks >= 90):
+     print("Grade : A")
+if(marks >= 80 and marks < 90):
+    print("Grade : B")
+if(marks >= 70 and marks <80):
+    print("Grade : C")    
+else:
+    print("Grade : D")
+
+     #or
+
 Marks=int(input("Enter your Marks : "))
 if(Marks>=90):
     print("Grade : A")
-elif(Marks<=90 and Marks>=80):
-    print("Grade : B")
-elif(Marks<=80 and Marks>=70):
+elif(90>Marks>=80):
+     print("Grade : B")
+elif(80>Marks>=70):
     print("Grade : C")
 else:
     print("Grade : D")
+
+
+
+
+Marks=int(input("Enter your Marks : "))
+if(Marks>=90):
+    print("Grade : A")
+elif(Marks<90 and Marks>=80):
+    print("Grade : B")
+elif(Marks<80 and Marks>=70):
+    print("Grade : C")
+else:
+    print("Grade : D")
+
+       #or
+
+Marks=int(input("Enter your Marks : "))
+if(Marks>=90):
+    print("Grade : A")
+if(Marks<90 and Marks>=80):
+    print("Grade : B")
+if(Marks<80 and Marks>=70):
+    print("Grade : C")
+else:
+    print("Grade : D")
+
 
 
 #if-elif-else
@@ -28,5 +72,16 @@ if(food=="cake" or food=="jalebi"):
 else:
     print("no sweets")
 
+
+
+#nesting - one statement inside another
+age = 34
+if(age>=18):
+    if(age>=80):
+        print("cannot drive")
+    else:
+        print("can Drive")
+else:
+    print("cannot Drive")
 
 
