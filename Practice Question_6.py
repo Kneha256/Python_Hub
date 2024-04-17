d6486477a7e642a9ec844004c9e98e531ed97b86
Kumari Neha <158587756+Kneha256@users.noreply.github.com>
2024-04-17 15:43:15 +0530

    Add files via upload

--- Practice Question_6.py
+++ Practice Question_6.py
@@ -1,1 +1,37 @@
+#WAP to find the of first two n natural number using while loop.
+n = 5
+sum = 0
+for el in range(1, n+1):
+    sum+=el
+    print("total sum = ",sum)
+
+n = 5
+sum = 0
+num = 1
+while num<=n:
+    sum+=num
+    num+=1
+    print(sum)
+
+#WAP to find the factorial of first n number using for loop.
+n = int(input("enter a num : "))
+sum = 1
+for el in range(1, n+1):
+    sum*=el
+    print("total sum = ",sum)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
