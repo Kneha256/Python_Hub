1f794ee16fc79ceaa2f614040eadb2f55dcf1ae7
Kumari Neha <158587756+Kneha256@users.noreply.github.com>
2024-05-02 08:57:35 +0530

    Update Practice Question_8.py

--- Practice Question_8.py
+++ Practice Question_8.py
@@ -1,1 +1,13 @@
+print(2**4) #16
 
+print("neha"+"kumari") #nehakumari
+print("neha","kumari") #neha kumari
+
+print(int(2+8.6)) #10
+print(2+8.6) #10.6
+
+print(10/2) #5.0
+print(10//2) #5
+
+print(12/5) #2.4
+print(12//5) #2
