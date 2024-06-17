aadd79f9b737d5e7ac5941f31c3eeca61a48f2df
Kumari Neha <158587756+Kneha256@users.noreply.github.com>
2024-06-17 21:54:56 +0530

    Update DataTypes & Variables.py

--- DataTypes & Variables.py
+++ DataTypes & Variables.py
@@ -51,3 +51,30 @@ print(type(age))
 #5)None-no value give to a location
 a = None
 print(type(a))
+
+#TYPES OF TOCKENS IN PYTHON
+#1)Identifiers
+#2)Literals
+#3)Operators
+#4)Keywords-reserved values
+#Can not be use as a identifier or variables
+#case Sensitive(upper case and lower case)
+
+#printsum
+a = 2
+b = 3
+sum = a + b
+print(sum)
+
+#printdiff
+a = 5
+b = 3
+diff = a - b
+print(diff)
+
+#5)Punctuators-(),[],{},#,@,etc.
+#they are symbols use to organise sentence structure in programming
+#=,-=,+=,/=,*=,//=,etc.
+#python is a implicit typed language(no need to tell type of the variables)
+#ex-java.cpp,c are explicit typed Languages.(int age = 23).
+
